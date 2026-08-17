0dd632370a0135d5f075ef5a62f1e3e657386b80
Mariam Ben Aissa <hakuna.digitalpro@gmail.com>
2026-08-17 15:06:31 +0100

    i18n(ar) : corrige les reliquats "محتوى" (Mohtawa) en "Kreatly" post-rebranding
    6 occurrences du nom d'app en arabe dataient d'avant le rebranding et
    n'avaient pas été alignées sur fr.json/en.json (onboarding, profil,
    invitation, guide piliers, export PDF, page personas).

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,7 +70,7 @@ export default async function RootLayout({
       lang={locale}
       dir={dir}
       data-theme={dataTheme}
-      data-app-version="v2r"
+      data-app-version="v2s"
       style={styleOverrides}
     >
       <body
